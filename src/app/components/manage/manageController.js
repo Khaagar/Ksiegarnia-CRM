621dd5766b463f7a02b57d3ed7f8be606486c28e
Khaagar <rexis.freshmaker@gmail.com>
2017-07-11 07:56:18 +0200

    asd

--- src/app/components/manage/manageController.js
+++ src/app/components/manage/manageController.js
@@ -3,13 +3,6 @@ export default class ManageController{
         this.ManageService = ManageService;
         this.tabs = this.ManageService.getTabs();
         this.data = [];
-<<<<<<< HEAD
-        };
-    
-
-    getData(category){
-        this.data = JSON.parse(localStorage.getItem(category));
-=======
         this.columnHeaders = [];
         }
     
@@ -19,7 +12,6 @@ export default class ManageController{
         for (var key in this.data[0]){
             this.columnHeaders.push(key);
         }  
->>>>>>> commit 10.07
     }
 }
 
